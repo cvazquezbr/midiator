0905c4f59ad46add5101a77014e4610c88c55948
cvazquezbr <76398762+cvazquezbr@users.noreply.github.com>
2025-10-04 11:19:32 -0300

    ;

--- src/components/FieldPositioner.jsx
+++ src/components/FieldPositioner.jsx
@@ -390,7 +390,7 @@ const FieldPositioner = ({
         aspectRatio: aspectRatio,
         width: '100%',
         maxWidth: '100%',
-        maxHeight: '100%',
+        maxHeight: '80vh',
         border: '2px solid #ddd',
         borderRadius: 2,
         overflow: 'hidden',
